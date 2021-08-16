76f86f8ad0b05a371d5f15a41ddb2fc9efe8845c
Sanket Shah <sanket.shah.ss1@contractors.roche.com>
2021-08-16 16:37:24 +0530

    Implemented downloading static content and added library to sample app to test the functionality

--- RocheCommonSDK/app/src/main/java/com/roche/roche/dis/MainActivity.kt
+++ RocheCommonSDK/app/src/main/java/com/roche/roche/dis/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.roche.roche.dis
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
 import androidx.navigation.NavOptions
 import androidx.navigation.findNavController
@@ -14,6 +18,7 @@ import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.material.navigation.NavigationView
 import com.roche.roche.dis.databinding.ActivityMainBinding
+import com.roche.roche.dis.staticcontent.DownloadStaticContent
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -60,8 +65,65 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 val options = NavOptions.Builder().setLaunchSingleTop(true).build()
                 findNavController(R.id.main_nav_host).navigate(action, options)
             }
+            R.id.menu_download_static_content -> {
+                if(isStoragePermissionGranted()) {
+                    downloadStaticContent()
+                }
+            }
         }
         binding.mainDrawerLayout.close()
         return true
+    }
+
+    private fun downloadStaticContent() {
+        Toast.makeText(this, "Start Downloading Content", Toast.LENGTH_LONG).show()
+        DownloadStaticContent.downloadToFileSystem(
+            this,
+            "https://passport-static-content.tpp1-dev.platform.navify.com/com.roche.nrm_passport/configuration/countries/dev_countries.json",
+            "dev_countries.json",
+            ::callback
+        )
+    }
+
+    private fun callback(isSuccess: Boolean) {
+        if (isSuccess) {
+            Toast.makeText(this, "Downloading content is successful", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "Downloading content is failed", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    // Storage Permissions
+    private val REQUEST_EXTERNAL_STORAGE = 1
+    private val PERMISSIONS_STORAGE = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+
+    private fun isStoragePermissionGranted(): Boolean {
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                PERMISSIONS_STORAGE,
+                REQUEST_EXTERNAL_STORAGE
+            )
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            REQUEST_EXTERNAL_STORAGE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    downloadStaticContent()
+                }
+            }
+        }
     }
 }
--- RocheCommonSDK/staticContent/src/main/java/com/roche/roche/dis/staticcontent/DownloadStaticContent.kt
+++ RocheCommonSDK/staticContent/src/main/java/com/roche/roche/dis/staticcontent/DownloadStaticContent.kt
@@ -1,39 +1,52 @@
 package com.roche.roche.dis.staticcontent
 
+import android.content.Context
 import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.BufferedInputStream
+import java.io.File
 import java.io.FileOutputStream
 import java.io.InputStream
 import java.io.OutputStream
-import java.lang.Exception
 import java.net.HttpURLConnection
 import java.net.URL
 
 object DownloadStaticContent {
-    fun downloadToFileSystem(url: String) {
-        try {
-            val url = URL(url)
-            val connection = url.openConnection() as HttpURLConnection
-            connection.connect()
+    fun downloadToFileSystem(context: Context, urlString: String, fileName:String, callback: (Boolean) -> Unit) {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val url = URL(urlString)
+                val connection = url.openConnection() as HttpURLConnection
+                connection.connect()
 
-            // download the file
-            val input: InputStream = BufferedInputStream(connection.inputStream)
+                // download the file
+                val input: InputStream = BufferedInputStream(connection.inputStream)
+                val path = context.getExternalFilesDir(null)?.absoluteFile?.path + File.separator + fileName
+                val output: OutputStream = FileOutputStream(path)
 
-            val path = "/sdcard/test.json"
-            val output: OutputStream = FileOutputStream(path)
+                val data = ByteArray(1024)
+                var count: Int
+                while (input.read(data).also { count = it } != -1) {
+                    output.write(data, 0, count)
+                }
 
-            val data = ByteArray(1024)
-            var count: Int
-            while (input.read(data).also { count = it } != -1) {
-                output.write(data, 0, count)
+                // close streams
+                output.flush()
+                output.close()
+                input.close()
+
+                withContext(Dispatchers.Main) {
+                    callback(true)
+                }
+            } catch (e: Exception) {
+                Log.e("DownloadStaticContent", "Exception ${e.localizedMessage}")
+                withContext(Dispatchers.Main) {
+                    callback(false)
+                }
             }
-
-            // close streams
-            output.flush()
-            output.close()
-            input.close()
-        } catch (e: Exception) {
-            Log.e("DownloadStaticContent", "Exception ${e.localizedMessage}")
         }
     }
 }
