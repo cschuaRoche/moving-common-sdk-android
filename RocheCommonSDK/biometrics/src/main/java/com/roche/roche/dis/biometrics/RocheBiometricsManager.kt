3fd66276633f78fbc9874879aafabeaf8c812d6b
kakadec <chandrashekhar.kakade@contractors.roche.com>
2021-09-02 19:48:50 +0530

    DHP-3536 added unit test cases for biometric

--- RocheCommonSDK/biometrics/src/main/java/com/roche/roche/dis/biometrics/RocheBiometricsManager.kt
+++ RocheCommonSDK/biometrics/src/main/java/com/roche/roche/dis/biometrics/RocheBiometricsManager.kt
@@ -41,7 +41,7 @@ class RocheBiometricsManager(
             hasMultipleBiometrics() -> {
                 BiometricsType.MULTIPLE
             }
-            isFaceSupported() -> {
+            isFingerprintSupported() -> {
                 BiometricsType.FINGERPRINT
             }
             isFaceSupported() -> {
