2f9076600a9cfa9d7e1c3d05a19bb984185a9adb
Christopher Chua <christopher.chua@roche.com>
2021-12-02 14:03:58 -0800

    DHP-4255 Fix for PR comments

--- RocheCommonSDK/app/src/main/java/com/roche/ssg/sample/security/ui/MySecurityCheckerFragment.kt
+++ RocheCommonSDK/app/src/main/java/com/roche/ssg/sample/security/ui/MySecurityCheckerFragment.kt
@@ -5,51 +5,73 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.roche.ssg.sample.BuildConfig
 import com.roche.ssg.sample.R
 import com.roche.ssg.sample.databinding.FragmentSecurityCheckerBinding
-import com.roche.ssg.security.presentation.SecurityCheckerFragment
 import com.roche.ssg.security.presentation.SecurityCheckerViewModel
+import com.roche.ssg.security.presentation.SecurityCheckerViewState
 
-class MySecurityCheckerFragment : SecurityCheckerFragment() {
+class MySecurityCheckerFragment : Fragment() {
     private lateinit var binding: FragmentSecurityCheckerBinding
+    private val securityViewModel : SecurityCheckerViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        securityViewModel = SecurityCheckerViewModel(requireActivity().application)
         binding = FragmentSecurityCheckerBinding.inflate(inflater, container, false)
-        resetTxtValidLicense()
         return binding.root
     }
 
-    override fun shouldValidateLicense(): Boolean {
-        return BuildConfig.ENABLE_SECURITY
-    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    override fun provideLicensingKey(): String {
-        return BuildConfig.LICENSE_KEY
-    }
-
-    override fun provideBaseUrl(): String {
-        return BuildConfig.BASE_URL
-    }
-
-    override fun isOfflineMode(): Boolean {
-        return BuildConfig.IS_OFFLINE_MODE
-    }
-
-    override fun onValidLicense() {
-        Log.d("MySecurityCheckerFragment", "onValidLicense")
-        if (BuildConfig.DEBUG) {
-            binding.txtValidLicense.setText(R.string.valid_license_debug)
+        securityViewModel.viewState.observe(this) {
+            Log.d("MySecurityCheckerFragment", "viewState: $it")
+            when (it) {
+                SecurityCheckerViewState.DeviceIsRooted -> {
+                    binding.txtValidLicense.setText(R.string.rooted_device_dialog_desc)
+                }
+                SecurityCheckerViewState.InvalidLicense -> {
+                    binding.txtValidLicense.setText(R.string.invalid_license_dialog_desc)
+                }
+                SecurityCheckerViewState.Retry -> {
+                    // take action if state is Retry, which usually means a network failure.
+                    // you may add retry logic and call securityViewModel.validate again.
+                    binding.txtValidLicense.setText(R.string.retry_security_check)
+                }
+                SecurityCheckerViewState.ValidLicense -> {
+                    // take action when license is valid
+                    // if shouldValidateLicense is false, the IgnoreLicenseCheck will trigger instead
+                    if (BuildConfig.DEBUG) {
+                        binding.txtValidLicense.setText(R.string.valid_license_debug)
+                    } else {
+                        binding.txtValidLicense.setText(R.string.valid_license)
+                    }
+                }
+                SecurityCheckerViewState.IgnoreLicenseCheck -> {
+                    binding.txtValidLicense.setText(R.string.security_check_ignored)
+                }
+            }
         }
-        binding.txtValidLicense.visibility = View.VISIBLE
     }
 
-    private fun resetTxtValidLicense() {
-        binding.txtValidLicense.visibility = View.GONE
-        binding.txtValidLicense.setText(R.string.valid_license)
+    override fun onResume() {
+        super.onResume()
+
+        // you should validate every time the UI is resumed
+        securityViewModel.validate(
+            BuildConfig.LICENSE_KEY,
+            BuildConfig.BASE_URL,
+            BuildConfig.ENABLE_SECURITY,
+            BuildConfig.IS_OFFLINE_MODE
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        securityViewModel.onDestroy() // make sure to call destroy to avoid memory leaks
     }
 }
