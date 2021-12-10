53345f11ac3f238b055334567f916ebae412ec57
kakadec <chandrashekhar.kakade@contractors.roche.com>
2021-12-10 18:47:11 +0530

    DHP-4248 featched User Info & displayed on UI

--- RocheCommonSDK/app/src/main/java/com/roche/ssg/sample/login/navify/NavifyLoginFragment.kt
+++ RocheCommonSDK/app/src/main/java/com/roche/ssg/sample/login/navify/NavifyLoginFragment.kt
@@ -10,16 +10,17 @@ import android.view.ViewGroup
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.roche.ssg.sample.R
 import com.roche.ssg.sample.databinding.FragmentNavifyLoginBinding
+import com.roche.ssg.sample.login.navify.vm.NavifyLoginViewModel
 
 class NavifyLoginFragment : Fragment() {
 
     private lateinit var mBinding: FragmentNavifyLoginBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        login()
-    }
+    private val mNavifyLoginViewModel: NavifyLoginViewModel by viewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,7 +28,20 @@ class NavifyLoginFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         mBinding = FragmentNavifyLoginBinding.inflate(inflater, container, false)
+        setLoginClickListener()
+        setViewStateObserver()
         return mBinding.root
+    }
+
+    private fun setLoginClickListener() {
+        mBinding.btnLogin.setOnClickListener {
+            val currentState = mNavifyLoginViewModel.navifyLoginViewState.value?.result
+            if (currentState is NavifyLoginViewModel.NavifyLoginResult.LoginPending
+                || currentState is NavifyLoginViewModel.NavifyLoginResult.LoginFailed
+            ) {
+                login()
+            }
+        }
     }
 
     private fun login() {
@@ -37,13 +51,48 @@ class NavifyLoginFragment : Fragment() {
     private var resultLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
             Log.i("Testing", "Result code ${result.resultCode}")
+            var authCode: String? = null
             if (result.resultCode == Activity.RESULT_OK) {
                 // There are no request codes
                 val data: Intent = result.data!!
-                val code = data.getStringExtra("auth_code")
-                //Log.i("Testing", "Received code $code")
+                authCode = data.getStringExtra("auth_code")
+                Log.i("Testing", "Received code $authCode")
             } else if (result.resultCode == Activity.RESULT_CANCELED) {
                 Log.i("Testing", "Authentication cancelled")
             }
+            mNavifyLoginViewModel.validateAuthCode(authCode)
         }
+
+    private fun setViewStateObserver() {
+        mNavifyLoginViewModel.navifyLoginViewState.observe(viewLifecycleOwner, {
+            when (it.result) {
+                is NavifyLoginViewModel.NavifyLoginResult.LoginPending -> {
+                    mBinding.txtStatus.text = getString(R.string.authenticate)
+                    mBinding.btnLogin.text = getString(R.string.login_to_navify)
+                    hideUserInfo()
+                }
+                is NavifyLoginViewModel.NavifyLoginResult.LoginSuccess -> {
+                    mBinding.txtStatus.text = getString(R.string.authentication_successful)
+                    mBinding.btnLogin.text = getString(R.string.logout)
+                    hideUserInfo()
+                }
+                is NavifyLoginViewModel.NavifyLoginResult.LoginFailed -> {
+                    mBinding.txtStatus.text = getString(R.string.authentication_failed)
+                    mBinding.btnLogin.text = getString(R.string.login_to_navify)
+                    hideUserInfo()
+                }
+                is NavifyLoginViewModel.NavifyLoginResult.LoginUserInfo -> {
+                    mBinding.flowUser.visibility = View.VISIBLE
+                    mBinding.user = it.result.user
+                }
+                is NavifyLoginViewModel.NavifyLoginResult.LoginUserInfoFailed -> {
+                    hideUserInfo()
+                }
+            }
+        })
+    }
+
+    private fun hideUserInfo() {
+        mBinding.flowUser.visibility = View.GONE
+    }
 }
---
+++ RocheCommonSDK/app/src/main/java/com/roche/ssg/sample/login/navify/vm/NavifyLoginViewModel.kt
@@ -0,0 +1,131 @@
+package com.roche.ssg.sample.login.navify.vm
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.gson.Gson
+import com.roche.ssg.sample.login.navify.data.Token
+import com.roche.ssg.sample.login.navify.data.UserInfo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.DataOutputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import java.nio.charset.StandardCharsets
+
+
+class NavifyLoginViewModel : ViewModel() {
+
+    val navifyLoginViewState = MutableLiveData<NavifyLoginViewState>()
+
+    init {
+        navifyLoginViewState.postValue(NavifyLoginViewState(NavifyLoginResult.LoginPending))
+    }
+
+    fun validateAuthCode(authCode: String?) {
+        if (authCode == null) {
+            navifyLoginViewState.postValue(NavifyLoginViewState(NavifyLoginResult.LoginFailed))
+        } else {
+            getToken(authCode)
+        }
+    }
+
+    private fun getToken(authCode: String) {
+
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                Log.i("Testing", "getting token")
+                val params =
+                    "code=$authCode&grant_type=authorization_code&redirect_uri=roche://com.roche.ssg/auth/callback&client_secret=aec38cd1-24fe-4ed9-8990-9af376100930&client_id=ssg-dev-reference-app-patient"
+                val postData: ByteArray = params.toByteArray(StandardCharsets.UTF_8)
+                val postDataLength = postData.size
+                val connection =
+                    URL("https://keycloak.appdevus.platform.navify.com/auth/realms/patients/protocol/openid-connect/token").openConnection() as HttpURLConnection
+                connection.requestMethod = "POST"
+                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
+                connection.setRequestProperty("charset", "utf-8")
+                connection.setRequestProperty("Content-Length", postDataLength.toString())
+
+                DataOutputStream(connection.outputStream).use { wr -> wr.write(postData) }
+
+                if (connection.responseCode >= HttpURLConnection.HTTP_OK
+                    && connection.responseCode < HttpURLConnection.HTTP_MULT_CHOICE
+                ) {
+                    val response = connection.inputStream.bufferedReader().readText()
+
+
+                    val token: Token = Gson().fromJson(
+                        response,
+                        Token::class.java
+                    )
+                    navifyLoginViewState.postValue(
+                        NavifyLoginViewState(
+                            NavifyLoginResult.LoginSuccess(token)
+                        )
+                    )
+
+                    Log.i("Testing", "Received Access token ${token.accessToken}")
+                    Log.i("Testing", "Received ID token ${token.idToken}")
+
+                    getUserInfo(token)
+                } else {
+                    navifyLoginViewState.postValue(
+                        NavifyLoginViewState(
+                            NavifyLoginResult.LoginFailed
+                        )
+                    )
+                    connection.errorStream.bufferedReader().readText()
+                }
+            }
+        }
+    }
+
+    private fun getUserInfo(token: Token) {
+
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                val connection =
+                    URL("https://keycloak.appdevus.platform.navify.com/auth/realms/patients/protocol/openid-connect/userinfo").openConnection() as HttpURLConnection
+                connection.requestMethod = "GET"
+                connection.setRequestProperty("charset", "utf-8")
+                connection.setRequestProperty("Authorization", "Bearer ${token.accessToken}")
+
+                if (connection.responseCode >= HttpURLConnection.HTTP_OK
+                    && connection.responseCode < HttpURLConnection.HTTP_MULT_CHOICE
+                ) {
+                    val response = connection.inputStream.bufferedReader().readText()
+                    val userInfo = Gson().fromJson(
+                        response,
+                        UserInfo::class.java
+                    )
+                    navifyLoginViewState.postValue(
+                        NavifyLoginViewState(
+                            NavifyLoginResult.LoginUserInfo(
+                                userInfo
+                            )
+                        )
+                    )
+                } else {
+                    connection.errorStream.bufferedReader().readText()
+                    navifyLoginViewState.postValue(
+                        NavifyLoginViewState(
+                            NavifyLoginResult.LoginUserInfoFailed
+                        )
+                    )
+                }
+            }
+        }
+    }
+
+    sealed class NavifyLoginResult {
+        object LoginPending : NavifyLoginResult()
+        class LoginSuccess(val token: Token) : NavifyLoginResult()
+        object LoginFailed : NavifyLoginResult()
+        class LoginUserInfo(val user: UserInfo) : NavifyLoginResult()
+        object LoginUserInfoFailed : NavifyLoginResult()
+    }
+}
+
+data class NavifyLoginViewState(val result: NavifyLoginViewModel.NavifyLoginResult)
